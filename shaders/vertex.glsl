a5100d616746c601af86a4f50f7a22df89c44e23
divmone <dimasston99@gmail.com>
2024-10-18 01:40:34 +0300

    crash

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,9 +1,27 @@
-#version 330 core
-layout (location = 0) in vec3 position;
+#version 430
 
-out vec3 FragPos;
+
+layout (location = 0) in vec4 pos;
+layout (location = 1) in vec3 color;
+
+layout(location = 2) uniform float multiplier;
+layout(location = 3) uniform float aX;
+layout(location = 4) uniform float aY;
+layout(location = 5) uniform float aZ;
+
+out  vec4 colors;
 
 void main()
 {
-    FragPos = vec3(0.3 * vec4(position, 1.0f));
-}
+        mat4 rotation = mat4(
+        cos(aZ), 0.0, sin(aZ), 0.0,
+        0.0,        1.0, 0.0,        0.0,
+       -sin(aZ), 0.0, cos(aZ), 0.0,
+        0.0,        0.0, 0.0,        1.0
+    );
+
+        vec3 rotatedPos = (rotation * vec4(pos.xyz, 1.0)).xyz;  // Вращаем позицию
+        vec3 transformedPos = rotatedPos * multiplier + vec3(aX, aY, 0.0); 
+        gl_Position = vec4(transformedPos, pos.w);
+        colors = vec4(color, 1.0);
+}     
