17937955107c6330bab1851a6b64c2f6f1431647
divmone <dimasston99@gmail.com>
2024-10-24 03:08:05 +0300

    fixfixfix

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,27 +1,18 @@
 #version 430
 
 
-layout (location = 0) in vec4 pos;
+layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 color;
 
-layout(location = 2) uniform float multiplier;
-layout(location = 3) uniform float aX;
-layout(location = 4) uniform float aY;
-layout(location = 5) uniform float aZ;
+
+layout(location = 2) uniform mat4 MVP;
 
 out  vec4 colors;
 
 void main()
 {
-        mat4 rotation = mat4(
-        cos(aZ), 0.0, sin(aZ), 0.0,
-        0.0,        1.0, 0.0,        0.0,
-       -sin(aZ), 0.0, cos(aZ), 0.0,
-        0.0,        0.0, 0.0,        1.0
-    );
 
-        vec3 rotatedPos = (rotation * vec4(pos.xyz, 1.0)).xyz;  // Вращаем позицию
-        vec3 transformedPos = rotatedPos * multiplier + vec3(aX, aY, 0.0); 
-        gl_Position = vec4(transformedPos, pos.w);
-        colors = vec4(color, 1.0);
-}     
+    gl_Position = MVP * vec4(pos, 1.0);
+
+    colors = vec4(color, 1.0);
+}
