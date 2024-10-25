f39ee5ea9980314d57bd50394240464f3c9dbcce
divmone <dimasston99@gmail.com>
2024-10-25 11:39:37 +0300

    ~

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,18 +1,18 @@
 #version 430
 
-
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 color;
+layout (location = 2) in vec3 normal;
 
+uniform mat4 MVP;
+uniform mat4 model;
 
-layout(location = 2) uniform mat4 MVP;
-
-out  vec4 colors;
+out vec3 Normal;
+out vec3 FragPos;
 
 void main()
 {
-
-    gl_Position = MVP * vec4(pos, 1.0);
-
-    colors = vec4(color, 1.0);
-}
+    gl_Position = MVP * vec4(pos, 1.0f);
+    FragPos = vec3(model * vec4(pos, 1.0f));
+    Normal = normal;  
+} 
