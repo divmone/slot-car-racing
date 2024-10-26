c756960f3a8f0772e0b02dab442a49b5ca34c4af
divmone <dimasston99@gmail.com>
2024-10-26 13:26:11 +0300

    ~~~

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -11,11 +11,9 @@ uniform vec3 objectColor;
 
 void main()
 {
-    // Ambient
     float ambientStrength = 0.1f;
     vec3 ambient = ambientStrength * lightColor;
   	
-    // Diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
@@ -28,5 +26,5 @@ void main()
     vec3 specular = specularStrength * spec * lightColor;  
         
     vec3 result = (ambient + diffuse + specular) * objectColor;
-    color = vec4(result, 1.0f);
+    color = vec4(result  , 1.0f);
 }
