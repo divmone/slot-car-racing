8a5ce0609aeaf8897eb09fc9f78a3210fa19825e
divmone <dimasston99@gmail.com>
2024-12-10 05:44:10 +0300

    !

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,10 +1,18 @@
 #version 430 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+  
 out vec4 color;
 
 in vec3 FragPos;  
 in vec3 Normal; 
 in vec2 tCoord; 
-  
+
+uniform Material material;
 uniform vec3 lightPos; 
 uniform vec3 viewPos;
 uniform vec3 lightColor;
@@ -13,21 +21,19 @@ uniform sampler2D tex;
 
 void main()
 {
-    float ambientStrength = 0.1f;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = lightColor * material.ambient;
   	
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    
-    float specularStrength = 0.5f;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 diffuse = lightColor * (diff * material.diffuse);
+
+   vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
-        
-    vec3 result = (ambient + diffuse + specular) * vec3(texture(tex, tCoord));
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = lightColor * (spec * material.specular);  
+
+    vec3 result = (ambient + diffuse + specular) * vec3(texture(tex, tCoord).rgb);
     color = vec4(result, 1.0f);
     //color = texture(tex, tCoord);
 }
